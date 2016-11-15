1b8847cc6b9d4ea255dc43fa6a9ef544fe3b6ad9
Samuel Buzas <buzassg@mail.uc.edu>
2016-11-15 17:12:51 -0500

    Added functions
    Added CreateWB and CreateWS

--- PyAcc.py
+++ PyAcc.py
@@ -7,10 +7,19 @@ from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
 
-# Creat Workbook
-def CreateWB(filename = "MyFirstWorkBook", title = "Worksheet 1"):
-	wb = Workbook()
+# Create Workbook
+#Supply a WorkBook/Project name. and a filename for it to be saved as
+wb = Workbook()
+def createWB(filename = "MyFirstWorkBook"):
 	dest_filename = filename + '.xlsx'
 	wb.save(filename = dest_filename)
 
+# Create WorkSheet
 
+def createWS(sheet_name = "Worksheet", filename = "MyFirstWorkBook"):
+	#do shome shite
+	dest_filename = filename + '.xlsx'
+	wb.save(filename = dest_filename)
+
+	wb.create_sheet(sheet_name)
+	wb.save(filename = dest_filename)
