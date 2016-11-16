9d7c59185994bcf2647cf57c618dd6fa18156e6e
Samuel Buzas <buzassg@mail.uc.edu>
2016-11-15 19:35:58 -0500

    Added Example of final goal/ Fixed PyAcc.py
    The included excel file is an example of what i think our final product
    could look like.
    
    Fixed the PyAcc creat_workbook functin. now properly creates a workbook
    of a specified name, and sets the intital load sheet to a default name
    other than 'Sheet'

--- PyAcc.py
+++ PyAcc.py
@@ -7,19 +7,19 @@ from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
 
-# Create Workbook
-#Supply a WorkBook/Project name. and a filename for it to be saved as
+#Constant
 wb = Workbook()
-def createWB(filename = "MyFirstWorkBook"):
+
+# Create Workbook
+def create_workbook(filename = "MyFirstBudget", sheet_name = "SimpleBudget"):
 	dest_filename = filename + '.xlsx'
+	wb.active.title = sheet_name
 	wb.save(filename = dest_filename)
 
 # Create WorkSheet
-
-def createWS(sheet_name = "Worksheet", filename = "MyFirstWorkBook"):
-	#do shome shite
+def create_worksheet(sheet_name = "Trackin Sheet", filename = "MyFirstBudget"):
 	dest_filename = filename + '.xlsx'
-	wb.save(filename = dest_filename)
-
 	wb.create_sheet(sheet_name)
 	wb.save(filename = dest_filename)
+
+# Create Graph
