eaff21c2e556ebf4d310861b890cb2ac391112c1
Samuel Buzas <buzassg@mail.uc.edu>
2016-11-28 15:19:23 -0500

    Update PyAcc.py
    Worked on create Transation in transaction class

--- PyAcc.py
+++ PyAcc.py
@@ -3,6 +3,7 @@
 # Desc: Contains Any functions or shortcut snippets used to sumplify main
 # Created By: Samuel Buzas & Elizabeth Sheetz
 #########
+import datetime
 from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
@@ -15,10 +16,14 @@ def create_workbook(filename = "MyFirstBudget", sheet_name = "SimpleBudget"):
 	dest_filename = filename + '.xlsx'
 	wb.active.title = sheet_name
 	wb.active['A1'] = 'Date'
+	wb.active.column_dimensions['A'].width = 10
 	wb.active['B1'] = 'Category'
+	wb.active.column_dimensions['B'].width = 10
 	wb.active['C1'] = 'Amount'
 	wb.active['D1'] = 'Check Number'
 	wb.active['E1'] = 'Description'
+	wb.active.column_dimensions['E'].width = 30
+	wb.geuss_types = True
 	wb.save(filename = dest_filename)
 
 # Create WorkSheet
@@ -30,12 +35,14 @@ def create_worksheet(sheet_name = "Trackin Sheet", filename = "MyFirstBudget"):
 # Create Graph Still Needed
 
 class Transaction:
+	
+	
 
-	entryNumber = 0
-
-	def __init__(self, date = [12,12,1212] , category = 'None' , amount = 0.0, checknum = 'None' , desc ='Ex. This was a purcahse'):
-		# Date formati is Month/ Day / Year
-		self.date = date
+	def __init__(self, Day = 12, Month =12, Year=12 , category = 'None' , amount = 0.0, checknum = 'None' , desc ='Ex. This was a purcahse'):
+		# Date format is Month/ Day / Year
+		self.Day = Day
+		self.Month = Month
+		self.Year = Year
 		#Category is used to lable puchases (i.e: Food, Entertainment, Gas, Travel,...etc)
 		self.category = category
 		# The Amount of the transaction
@@ -44,10 +51,22 @@ class Transaction:
 		self.checknum = checknum
 		# Short Description of the purpose of the transaction, further defines the transaction with help of category
 		self.desc = desc
+		# excel isnt 0 based and row 1 is for titles
+		self.entryNumber = 2
 	
 	# Create Entry
-	def createTransaction(self, date = [12,12,1212] , category = 'None' , amount = 0.0, checknum = 'None' , desc ='Ex.This was a purcahse'):
-		pass
+	def createTransaction(self, day= 12, month= 12, year= 12 , category = 'None' , amount = 0.0, checknum = 'None' , desc ='Ex.This was a purcahse'):
+		sheet = wb.active
+		sheet['A' + str(self.entryNumber)] = datetime.datetime(year, month, day)
+		sheet['A' + str(self.entryNumber)].number_format
+		wb.geuss_types= True
+		sheet['B' + str(self.entryNumber)] = category
+		sheet['C' + str(self.entryNumber)] = amount
+		sheet['D' + str(self.entryNumber)] = checknum
+		sheet['E' + str(self.entryNumber)] = desc
+
+		wb.save('MyFirstBudget.xlsx')
+		self.entryNumber += 1
 
 # Edit Entry
 
